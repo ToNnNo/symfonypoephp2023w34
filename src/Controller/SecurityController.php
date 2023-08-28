8221b68e97981cbffc120bc600afdca2fb2df0b1
Stéphane Menut <smenut@dawan.fr>
2023-08-28 12:23:24 +0200

    security + form authentication

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,16 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'security_login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // dernier identifiant saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
 
+        // dernière erreur d'authentification
+        $error = $authenticationUtils->getLastAuthenticationError();
 
         return $this->render('security/login.html.twig', [
-
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
     }
 
