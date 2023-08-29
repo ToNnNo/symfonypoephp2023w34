3ede9c1dc1d409616dde85a79ba3b96400ce2c9d
Stéphane Menut <smenut@dawan.fr>
2023-08-29 14:30:24 +0200

    traduction fin

--- src/Controller/TranslationController.php
+++ src/Controller/TranslationController.php
@@ -7,10 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-#[Route('/{_locale}/translation', name: 'translation_')]
+#[Route('', name: 'translation_')]
 class TranslationController extends AbstractController
 {
-    #[Route('', name: 'index')]
+    #[Route('/{_locale}/translation', name: 'index')]
     public function index(TranslatorInterface $translator): Response
     {
         $welcome = $translator->trans('welcome in our app');
@@ -18,5 +18,15 @@ class TranslationController extends AbstractController
         return $this->render('translation/index.html.twig', [
             'welcome' => $welcome
         ]);
+    }
+
+    #[Route(path: [
+        'en' => '/about-us',
+        'fr' => '/a-propos'
+    ], name: 'about')]
+    public function about(): Response
+    {
+
+        return $this->render('translation/about.html.twig');
     }
 }
